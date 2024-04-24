8be67f836cbc36449986b8ba76615bf88ea9a3e3
Guilherme Heinrichs <guilhermeheinrichs@MacBook-Pro-de-Guilherme.local>
2024-04-24 17:04:56 -0300

    feat: Added  to submit search button

--- app/searchFilter.js
+++ app/searchFilter.js
@@ -1,12 +1,16 @@
 const searchElement = document.getElementById("search-button");
+const containerItemElement = document.getElementById("content");
+const searchInputElement = document.getElementById("search-input");
+
 searchElement.addEventListener('click', applySearchFilter);
 
 function applySearchFilter(){
-  const searchInputValue = document.getElementById("search-input").value;
-
+  searchElement.classList.add("is-loading");
   const filteredJobs = jobs.filter((job) =>
-  job.jobTitle.includes(searchInputValue));
-  const containerItemElement = document.getElementById("content");
+  job.jobTitle.includes(searchInputElement.value));
+  searchInputElement.focus();
   containerItemElement.innerHTML = ``;
+  if(!filteredJobs) getInformations();
   listAllJobOffers(filteredJobs);
+  searchElement.classList.remove("is-loading");
 }
